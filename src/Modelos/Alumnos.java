43e32efadab2606992895b7b49e7a4e098887ede
Carlos <cmchamorro99@hotmail.com>
2019-03-05 19:43:25 -0600

    Agregando Alumnos

--- src/Modelos/Alumnos.java
+++ src/Modelos/Alumnos.java
@@ -1,10 +1,40 @@
 package Modelos;
 
 public class Alumnos extends Persona{
-    String nombre, apellido;
+    private String nombre, apellido, numeroCuentaAlumno;
+
+    public Alumnos(String nombre, String apellido, String numeroCuentaAlumno){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numeroCuentaAlumno = numeroCuentaAlumno;
+    }
 
     @Override
     public void describirPersona() {
 
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNumeroCuentaAlumno() {
+        return numeroCuentaAlumno;
+    }
+
+    public void setNumeroCuentaAlumno(String numeroCuentaAlumno) {
+        this.numeroCuentaAlumno = numeroCuentaAlumno;
     }
 }
