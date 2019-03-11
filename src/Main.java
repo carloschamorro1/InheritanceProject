9b0cce913e3a4b337d6c74dfbc8d8a1e5273359b
Carlos <cmchamorro99@hotmail.com>
2019-03-11 17:41:52 -0600

    Eliminando comentarios

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,6 @@ public class Main {
         Run programa = new Run();
         personManager listaprincipal = new personManager();
         programa.run(listaprincipal);
-//        ArrayList<Persona> personas = new ArrayList<Persona>();
-//        personas.add(new Alumnos("Carlos", "Chamorro", 2017110190));
-//        personas.add(new Alumnos("Carlos", "Chamorro", 2017110190));
-//        personas.add(new Maestro("Juan", "rtyui", "Dr", 2017120565));
-//        personas.add(new Maestro("Juan", "rtyui", "Dr", 2017120565));
-//        for (int i = 0; i < personas.size(); i++) {
-//            personas.get(i).describirPersona();
-//        }
-
     }
 
 }
